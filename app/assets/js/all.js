0f894414356835a6d9fab5d0e512f9aa4e9849dd
Vivian <vivianhuang197@gmail.com>
2022-08-28 17:46:15 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,45 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+
+// code swiper
+var swiper = new Swiper(".codeSwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  loop: true,
+  loopedSlides: 15,
+  grabCursor: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0, //讓自動播放不延遲
+    waitForTransition: false //不等待頁面transition 持續播放
+  },
+
+
+
+
 });
+
+var swiper = new Swiper(".commentSwiper", {
+  //cssMode: true,
+  // Optional parameters
+  effect: 'fade',
+  direction: 'horizontal',
+  loop:true,
+  fadeEffect: {
+  crossFade: true
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+
